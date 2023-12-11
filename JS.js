222b8ce30985fbd2d5f58fc4b55dc00b860cc797
David huang <142414465+chunshao31@users.noreply.github.com>
2023-12-11 15:12:35 -0500

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,8 @@
+window.onload = function confirmpop(){
+  var result = window.confirm("The FBI wants your location!")
+}
+if (result == OK){
+} 
+else {
+  window.close();
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+var score = 0;
+function getRadioValue(){
+    var nm = $('#n').attr('name');
+    var radio = document.querySelector("input[name='nm']:checked");
+    console.log(n);
+    console.log(radio.value);
+    
+}
